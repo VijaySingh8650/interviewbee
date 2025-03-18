6674070ad3cc87d4815733bd506af272b3eb7587
VijaySingh@8650 <rathorevijay995@gmail.com>
2025-03-18 12:52:20 +0530

    issue-fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,24 @@
 "use client"; 
 
+import { useEffect, useState } from "react";
 import { useSession} from "next-auth/react";
 import LoginComponent from "./components/login";
 import MeetComponent from "./components/meet";
 
 export default function UserInfo() {
 
+  const [client, setClient] = useState(false);
+
+
   const { data: session } = useSession();
+  
+
+  useEffect(() => {
+    setClient(true); // hydration error
+  }, []);
 
 
 
-  return !session ?  <LoginComponent/> : <MeetComponent/>;
+
+  return (client ? (!session ?  <LoginComponent/> : <MeetComponent/>) : null);
 }
