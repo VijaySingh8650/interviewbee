39996b23a6e3d4a37da81824e278c8933e965c23
VijaySingh@8650 <rathorevijay995@gmail.com>
2025-03-19 18:13:03 +0530

    issue-fixed

--- src/app/components/scheduled-time.tsx
+++ src/app/components/scheduled-time.tsx
@@ -1,12 +1,12 @@
-import { RootState } from '@/store';
-import React from 'react'
-import {useSelector} from 'react-redux';
-import { Files } from 'lucide-react';
+import { RootState } from "@/store";
+import React from "react";
+import { useSelector } from "react-redux";
+import { Files } from "lucide-react";
 
 const ScheduledTime = () => {
-
-  const {time, day, month, year, dayNumber, link} = useSelector((state:RootState) => state.meet);
-  
+  const { time, day, month, year, dayNumber, link } = useSelector(
+    (state: RootState) => state.meet
+  );
 
   const copyToClipboard = async () => {
     try {
@@ -17,19 +17,33 @@ const ScheduledTime = () => {
     }
   };
 
-  return ((month && year) ?
-    <div className='w-[500px] shadow-meetingSchedule p-4 bg-white rounded-md text-center mt-12 m-auto'>
+  return month && year ? (
+    <div className="w-[500px] shadow-meetingSchedule p-4 bg-white rounded-md text-center mt-12 m-auto">
+      <h1 className="subheading-fontSize">Your meeting is scheduled</h1>
+      {
+        !link ? <p>Loading...</p> :
+        <>
+          <h1 className="subparagraph-fontSize mt-2">
+            {day +
+              ", " +
+              dayNumber +
+              " " +
+              month +
+              " " +
+              year +
+              " at " +
+              time.substring(0, 5)}
+          </h1>
+          <div className="flex items-center justify-between mt-4 p-2 bg-gray-300">
+            <a href={link} target="_blank" rel="noopener noreferrer">
+              {link}
+            </a>
+            <Files onClick={copyToClipboard} className="cursor-pointer" />
+          </div>
+        </>
+      }
+    </div>
+  ) : null;
+};
 
-        <h1 className='subheading-fontSize'>Your meeting is scheduled</h1>
-        <h1 className='subparagraph-fontSize mt-2'>{day+", "+dayNumber+" "+month+" "+year+" at "+ time.substring(0, 5)}</h1>
-        <div className='flex items-center justify-between mt-4 p-2 bg-gray-300'>
-          <a href={link} target="_blank" rel="noopener noreferrer" >{link}</a>
-          <Files  onClick={copyToClipboard} className='cursor-pointer'/>
-        </div>
-
-      
-    </div> : null
-  )
-}
-
-export default ScheduledTime
+export default ScheduledTime;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,20 @@
 "use client"; 
 
-import { useEffect, useState } from "react";
 import { useSession} from "next-auth/react";
 import LoginComponent from "./components/login";
 import MeetComponent from "./components/meet";
 
 export default function UserInfo() {
 
-  const [client, setClient] = useState(false);
+
 
 
   const { data: session } = useSession();
   
 
-  useEffect(() => {
-    setClient(true); // hydration error
-  }, []);
 
 
 
 
-  return (client ? (!session ?  <LoginComponent/> : <MeetComponent/>) : null);
+  return (!session ?  <LoginComponent/> : <MeetComponent/>);
 }
